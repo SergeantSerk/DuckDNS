55afebcf56aadc664a632e9447f7e8ae6330378d
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2021-11-13 23:09:42 +0000

    Reverted back to previous WebRequest
    Issue was with Docker, iptables and UFW's before6.rules.
    ```
    # START DOCKER IPv6
    *nat
    :POSTROUTING ACCEPT [0:0]
    -A POSTROUTING -s fd00::/80 ! -o docker0 -j MASQUERADE
    COMMIT
    # END DOCKER IPv6
    ```

--- DuckDNS/RequestHandler.cs
+++ DuckDNS/RequestHandler.cs
@@ -1,6 +1,6 @@
-﻿using System.IO;
+﻿using DuckDNS.Extension;
+using System.IO;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DuckDNS
@@ -9,14 +9,8 @@ namespace DuckDNS
     {
         public static async Task<string> GetAsync(string uri)
         {
-            /*HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            using HttpWebResponse response = (HttpWebResponse)await request.GetResponseNoExceptionAsync();
-            using Stream stream = response.GetResponseStream();
-            using var reader = new StreamReader(stream);
-            return await reader.ReadToEndAsync();*/
-
             HttpWebRequest request = WebRequest.CreateHttp(uri);
-            using WebResponse response = await request.GetResponseAsync();
+            using HttpWebResponse response = (HttpWebResponse)await request.GetResponseNoExceptionAsync();
             using Stream stream = response.GetResponseStream();
             using var reader = new StreamReader(stream);
             return await reader.ReadToEndAsync();
